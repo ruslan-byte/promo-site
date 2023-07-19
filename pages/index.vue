d0fcaf631dc22588190ade000b75581f74069576
ruslan-byte <rbaroev0@gmail.com>
2023-07-19 09:14:54 +0300

    Настроил типографию

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <Tutorial/>
+	<div>
+		<h2>Heading 1</h2>
+		<h3>Heading 2</h3>
+		<h4>Heading 3</h4>
+		<h5>Heading 4</h5>
+		<h6>Heading 5</h6>
+		<p>With skills in over 4 different fields of design, I am the perfect person to hire when it comes to a full fledged project. Whatever your needs are, I can pretty much take on any challenge.</p>
+	</div>
 </template>
 
 <script>
